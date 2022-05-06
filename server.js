d808cad8de5de41c1479d57003a3000006fe58d2
Leonardo Bittencourt <leonardo1silva2@gmail.com>
2022-05-06 15:07:49 -0300

    add limit

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
 
-app.get('/', async (req, res) => {
+app.get('/api', async (req, res) => {
 
   var q = req.query.q;
+  var limit = req.query.limit;
 
   const QY_SLCT_ITEM = 'div.rllt__details';
   const QY_SLCT_NAME = 'div.SPZz6b>h2>span';
@@ -40,7 +41,7 @@ app.get('/', async (req, res) => {
   while(true) {
     elements = elements = {} ? await page.$$(QY_SLCT_ITEM) : elements;
     try {await elements[item].click()} catch (e) {break}
-    await page.waitForTimeout(1050);
+    await page.waitForTimeout(900);
 
     try {var name = await page.$eval(QY_SLCT_NAME, (el) => el.textContent)} catch (e) {var name = ''}
     try {var address = await page.$eval(QY_SLCT_ADRESS, (el) => el.textContent)} catch (e) {var addres = ''}
@@ -54,6 +55,8 @@ app.get('/', async (req, res) => {
 
     [name, address, phone] = ['','',''];
 
+    if (data.length == limit) {break}
+
     if (item < 20) {
       item++
     }
@@ -61,7 +64,7 @@ app.get('/', async (req, res) => {
     if (item == 20) {
       try {await page.click(QY_SLCT_NEXT)} catch (e) {break};
       await page.waitForTimeout(2000);
-      await page.evaluate(()=>{CleanElements()})
+/*       await page.evaluate(()=>{CleanElements()})*/
       item = 0;
     }
 
@@ -71,9 +74,10 @@ app.get('/', async (req, res) => {
   await browser.close();
 });
 
-const port = 3000;
+const port = 4000;
 app.listen(port, () => {
   console.log(`
-    Servidor iniciado em: http://localhost:${port}
+    Servidor iniciado!
+    Exemplo de uso: http://localhost:${port}/api?q=igrejas&limit=10
   `)
 });
